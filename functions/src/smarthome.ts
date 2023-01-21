8f7bbcdefbb5701a5034daa101e3bccd45177bcd
Vincent Lam <vincent_lam_2@sfu.ca>
2023-01-20 16:44:55 -0800

    WIP: Added google funcitons auth

--- functions/src/smarthome.ts
+++ functions/src/smarthome.ts
@@ -1,24 +1,23 @@
-import { readFileSync } from 'fs';
-import { join } from 'path';
-
-import {
-    smarthome as SmartHome,
-    // SmartHomeV1ExecuteResponseCommands,
-    // Headers,
-  } from 'actions-on-google';
+import { OAuth2Client, TokenPayload } from 'google-auth-library';
+import { google } from 'googleapis'
 import * as functions from 'firebase-functions';
 
-let jwt;
-try {
-  jwt = JSON.parse(
-    readFileSync(join(__dirname, 'smart-home-key.json')).toString()
-  );
-} catch (e) {
-  functions.logger.warn('error reading service account key:', e);
-  functions.logger.warn('reportState and requestSync operation will fail');
+const CLIENT_ID = '491780931767-oohtjepvj9hk5prqicv3akh154cg16m7.apps.googleusercontent.com;'
+
+export const homegraph = google.homegraph("v1");
+const authClient = new OAuth2Client(CLIENT_ID);
+
+export async function verify(token: string) : Promise<TokenPayload | undefined> {
+  const ticket = await authClient.verifyIdToken({
+    idToken: token,
+    audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
+    // Or, if multiple clients access the backend:
+    //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+  });
+  google.options({auth: authClient});
+  return ticket.getPayload();
+  // If request specified a G Suite domain:
+  // const domain = payload['hd'];
 }
 
-export const smarthome = SmartHome({
-    jwt,
-  });
 
